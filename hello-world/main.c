d4ee922adc8ec113b798cafbf0339a33eaec972b
Edgar Herrera <edgarherrera@hey.com>
2021-07-10 16:37:46 -0500

    Initial commit.

---
+++ hello-world/main.c
@@ -0,0 +1,12 @@
+/* Pre-processors commands: Include libraries. */
+#include <stdio.h>
+
+/* Main function, all code is written here. */
+int main()
+{
+  /* printf: function that print on terminal a message  */
+  printf("Hello World!");
+
+  /* Exit program */
+  return 0;
+}
