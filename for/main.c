5f6634476dbc5a2d25b557c7cefef7cee6b6e40f
Edgar Herrera <edgarherrera@hey.com>
2021-07-11 12:42:53 -0500

    For.

---
+++ for/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+  int n = 30;
+  int i;
+  for (i = 0; i < n; i++)
+  {
+    printf("The current value for i is %d \n", i);
+    /* code */
+  }
+  
+  return 0;
+}
