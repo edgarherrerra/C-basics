83b9290433617a3dadd0c4fd6c2998ef45b0d6c9
Edgar Herrera <edgarherrera@hey.com>
2021-07-10 21:30:42 -0500

    Basic types.

---
+++ basic-types/main.c
@@ -0,0 +1,31 @@
+/* * 
+    * Basic Types [Arithmetics]
+    * (A) Integer:
+        * char 1 Byte -128 to 127
+            * unsigned char 1 Byte 0 to 255
+            
+        * int: It can be 2 or 4 bytes, it allows to have negative and positive numbers but no decimal values.
+            * 2 Bytes -32,768 to 32,757
+            * 4 bytes -2,147,483,648 to 2,147,483
+            * unsigned int: it can be 2 or 4 bytes, it doesn't allow negative data.
+                * 2 Bytes 0 to 65.535
+                * 4 bytes 0 to 4,294,967,295
+                
+        * short 2 bytes
+            * 2 bytes -32,768 to 32,767
+            * unsigned short 2 bytes 0 to 65,535
+
+        * long 8 Bytes
+            * long 8 bytes -9223372036854775808 to 9223372036854775807
+            * unsigned long 8 bytes 0 to 18446744073709551615
+
+    * (B) Floating point:
+        * float 4 bytes 1.2E-38 to 3.4E+386 (6 decimal places)
+        * double 8 bytes 2.3E-308 to1.7E+30815 (15 decimal places)
+        * long double 10 bytes 3.4E-4932 to 1.1E+493219 (19 decimal places) 
+        
+    * (C) Void: The void type specifies that no value is available. It is used in three kinds of situations:
+        * Function returns as void
+        * Function arguments as void
+        * Pointers to void
+*/
