e606cae07dcef27c9cbe6918374b6db426c35889
Edgar Herrera <edgarherrera@hey.com>
2021-07-13 23:33:26 -0500

    C libraries

---
+++ libraries/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <ctype.h>
+#include <signal.h>
+#include <locale.h>
+#include <errno.h>
+#include <assert.h>
